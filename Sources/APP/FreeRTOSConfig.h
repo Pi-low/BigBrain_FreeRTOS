3144361b4af561e992d1239a8b5020cf5fce40f8
Nello <nello.chom@protonmail.com>
2024-11-18 17:14:36 +0100

    Add FreeRTOS-Plus-CLI

--- Sources/APP/FreeRTOSConfig.h
+++ Sources/APP/FreeRTOSConfig.h
@@ -43,36 +43,37 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION			1
-#define configUSE_IDLE_HOOK				0
-#define configUSE_TICK_HOOK				0
-#define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
-#define configCPU_CLOCK_HZ				( ( unsigned long ) 32000000 )  /* Fosc / 2 */
-#define configMAX_PRIORITIES			( 4 )
-#define configMINIMAL_STACK_SIZE		( 256 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) 5120 )
-#define configMAX_TASK_NAME_LEN			( 4 )
-#define configUSE_TRACE_FACILITY		0
-#define configUSE_16_BIT_TICKS			1
-#define configIDLE_SHOULD_YIELD			1
+#define configUSE_PREEMPTION				1
+#define configUSE_IDLE_HOOK					0
+#define configUSE_TICK_HOOK					0
+#define configTICK_RATE_HZ					( ( TickType_t ) 1000 )
+#define configCPU_CLOCK_HZ					( ( unsigned long ) 32000000 )  /* Fosc / 2 */
+#define configMAX_PRIORITIES				( 4 )
+#define configMINIMAL_STACK_SIZE			( 256 )
+#define configTOTAL_HEAP_SIZE				( ( size_t ) 5120 )
+#define configMAX_TASK_NAME_LEN				( 4 )
+#define configUSE_TRACE_FACILITY			0
+#define configUSE_16_BIT_TICKS				1
+#define configIDLE_SHOULD_YIELD				1
+#define configCOMMAND_INT_MAX_OUTPUT_SIZE 	16
 
 /* Co-routine definitions. */
-#define configUSE_CO_ROUTINES 		1
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configUSE_CO_ROUTINES 				1
+#define configMAX_CO_ROUTINE_PRIORITIES 	( 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet		1
-#define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete				1
-#define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend			1
-#define INCLUDE_vTaskDelayUntil			1
-#define INCLUDE_vTaskDelay				1
+#define INCLUDE_vTaskPrioritySet			1
+#define INCLUDE_uxTaskPriorityGet			0
+#define INCLUDE_vTaskDelete					1
+#define INCLUDE_vTaskCleanUpResources		0
+#define INCLUDE_vTaskSuspend				1
+#define INCLUDE_vTaskDelayUntil				1
+#define INCLUDE_vTaskDelay					1
 
 
-#define configKERNEL_INTERRUPT_PRIORITY	0x01
+#define configKERNEL_INTERRUPT_PRIORITY		0x01
 #define pdMS_TO_TICKS( xTimeInMs ) ( ( TickType_t) ( ( ( TickType_t) ( xTimeInMs ) )) * ((( TickType_t) configTICK_RATE_HZ ) / ( TickType_t) 1000 ))
 
 #endif /* FREERTOS_CONFIG_H */
